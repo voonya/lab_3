2feb85d9fc6a341312470c13d01481834e20c41e
Ilya Knyazev <kknyaz033@gmail.com>
2021-03-13 20:26:08 +0200

    header filled

--- lab_3/Header.h
+++ lab_3/Header.h
@@ -1,1 +1,40 @@
-#pragma once
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <math.h>
+namespace fs = std::filesystem;
+using namespace std;
+
+class Node {
+public:
+    string key;
+    string value;
+    Node* next;
+
+    Node(string key, string value);
+};
+
+class HashTable {
+private:
+    int TABLE_SIZE = 1000;
+    Node** table;
+
+public:
+    HashTable();
+    ~HashTable();
+    int hashing(string key);
+    void insert(string key, string value);
+    string search(string key);
+    void show_table();
+};
+
+struct definition {
+    string key;
+    string value;
+};
+
+void work_loop(HashTable table, string key);
+string get_working_dir();
+void parse_file(string path, HashTable& table);
+definition parse_line(string line);
